f68b917d2c6bc8e90934c3f666715592852847f1
Abhishek53 <abhishekr.ar53@gmail.com>
2021-04-07 20:52:13 +0530

    test tools and package artifacts added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="wineq_package",
+    author=["abhishekr.ar53@gmail.com"],
+    packages=find_packages(),
+    licennse="MIT"
+)
